dbb5ed4ebe9521bc869b86d3475743fdc1836edd
DrRichAdams <proxyserver7798@gmail.com>
2024-05-18 21:15:17 +0200

    Added highlights component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import FAQ from "@/components/FAQ/faq";
+import Highlights from "@/components/highlights/highlights";
 import ResultsWinnings from "@/components/results_winnings/results_winnings";
 import React from "react";
 
 export default function Home() {
   return (
     <main className="main_container">
+      <Highlights />
       <ResultsWinnings />
       <FAQ />
     </main>
---
+++ src/components/highlights/highlights.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import React, {useState} from 'react';
+import "./highlights.css";
+import { TiArrowRight } from "react-icons/ti";
+
+const Highlights = () => {
+    const [ selected_highlight, set_selected_highlight ] = useState("football");
+    const [ selected_league, set_selected_league ] = useState(1);
+    const [ leages, set_leagues ] = useState([
+       { id: 1, title: "All" }, { id: 2, title: "Premier League" },
+       { id: 3, title: "Bundesliga" }, { id: 4, title: "English Premiership" },
+       { id: 5, title: "Serie A" }, { id: 6, title: "Ligue 1" }, { id: 7, title: "La Liga" },
+    ]);
+    const changeHighlight = (high_light: string) => {
+        set_selected_highlight(high_light)
+    }
+    const changeLeague = (id: number) => {
+        set_selected_league(id)
+    }
+  return (
+    <div className='highlights_container'>
+        <h3 className="high_title">Highlights</h3>
+        <div className="high_select_box">
+            <div className="high_select_btns">
+                <button style={{ backgroundColor: selected_highlight == "football"?"#B1BF24":"transparent"}} onClick={() => changeHighlight("football")}>Football</button>
+                <button style={{ backgroundColor: selected_highlight == "survival"?"#B1BF24":"transparent"}} onClick={() => changeHighlight("survival")}>Survival pool</button>
+            </div>
+            <button className='high_see_all'>
+                <p>see all</p>
+                <TiArrowRight size={23} />
+            </button>
+        </div>
+        <div className="high_filter_menu">
+            { leages.map((item) => (<button style={{ backgroundColor: item.id==selected_league? "#1F1F1F":"transparent" }} onClick={() => changeLeague(item.id)}>{item.title}</button>)) }
+        </div>
+        <div className="actual_highlights_container"></div>
+    </div>
+  )
+}
+
+export default Highlights
