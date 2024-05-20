a1dcf9ba4b5cfec14926d2bccd76d9e7c63b036f
DrRichAdams <proxyserver7798@gmail.com>
2024-05-20 17:34:45 +0200

    end of day commit

--- src/components/banner/main_banner.tsx
+++ src/components/banner/main_banner.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import "./banner.css";
 import Image from 'next/image';
@@ -6,33 +8,61 @@ import manu from "../../../public/pngs/manu.png";
 import chele from "../../../public/pngs/chelsea.png";
 import { LuTimer } from "react-icons/lu";
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 const MainBanner = () => {
   return (
-    <div className='main_banner_container'>
-        <Image src={banner_img} alt='banner img' className='banner_image' />
-        <div className="mb_titles">
-            <div className="mb_teams_box">
-                <div className="mb_team_box"><Image src={manu} alt='team' style={{ width: "40px" }} /></div>
-                <div className="mb_team_versus">V</div>
-                <div className="mb_team_box"><Image src={chele} alt='team' style={{ width: "40px" }} /></div>
-            </div>
-            <div className="mb_time_box">
-                <LuTimer color='#fff' size={20} />
-                <p>Today at 6:30 AM</p>
-            </div>
-        </div>
-        <div className="mb_details">
-            <h3>Premier League</h3>
-            <h1>Manchester United vs Chelsea FC</h1>
-            <p>
-                Get in on the action! Bet on today's match for instant cashback and
-                 unlock special bonuses just for our players."
-            </p>
-            <button className="mb_btn_cta">Bet Now</button>
-        </div>
-        <div className="mb_pagination"></div>
-    </div>
+    <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide><SwiperElement /></SwiperSlide>
+      <SwiperSlide><SwiperElement /></SwiperSlide>
+      <SwiperSlide><SwiperElement /></SwiperSlide>
+      <SwiperSlide><SwiperElement /></SwiperSlide>
+      ...
+    </Swiper>
   )
+}
+
+const SwiperElement = () => {
+    return(
+        <div className='main_banner_container'>
+            <Image src={banner_img} alt='banner img' className='banner_image' />
+            <div className="mb_titles">
+                <div className="mb_teams_box">
+                    <div className="mb_team_box"><Image src={manu} alt='team' style={{ width: "40px" }} /></div>
+                    <div className="mb_team_versus">V</div>
+                    <div className="mb_team_box"><Image src={chele} alt='team' style={{ width: "40px" }} /></div>
+                </div>
+                <div className="mb_time_box">
+                    <LuTimer size={20} />
+                    <p>Today at 6:30 AM</p>
+                </div>
+            </div>
+            <div className="mb_details">
+                <h3>Premier League</h3>
+                <h1>Manchester United vs Chelsea FC</h1>
+                <p>
+                    Get in on the action! Bet on today's match for instant cashback and
+                    unlock special bonuses just for our players."
+                </p>
+                <button className="mb_btn_cta">Bet Now</button>
+            </div>
+            <div className="mb_pagination"></div>
+        </div>
+    )
 }
 
 export default MainBanner
