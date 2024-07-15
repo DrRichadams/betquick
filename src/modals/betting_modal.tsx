74bb00bef4d8d3069c1db0569412df8314d19feb
DrRichadams <proxyserver7798@gmail.com>
2024-07-15 17:28:51 +0200

    final commit of the work day

--- src/modals/betting_modal.tsx
+++ src/modals/betting_modal.tsx
@@ -1,10 +1,11 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import styles from "./modal_styles.module.css";
 
 const BettingModal = () => {
+    const [ selected, setSelected ] = useState("football");
     const searchParams = useSearchParams()
     const isBetting = searchParams.get("isBetting")
     const router = useRouter()
@@ -18,7 +19,10 @@ const BettingModal = () => {
             return (
                 <div className={styles.modalOverlay} onClick={handleModalClose}>
                     <div className={styles.modalContainer} onClick={(e) => e.stopPropagation()}>
-        
+                        <div className={styles.bet_type_selector_box}>
+                            <button onClick={() => setSelected("football")} className={styles.bet_selector_btn} style={{ backgroundColor: selected=="football"? "#B1BF24":"transparent", color: selected=="football"? "#fff":"#B1BF24"  }}>Football</button>
+                            <button onClick={() => setSelected("survival")} className={styles.bet_selector_btn} style={{ backgroundColor: selected=="survival"? "#B1BF24":"transparent", color: selected=="survival"? "#fff":"#B1BF24" }}>Survival Pool</button>
+                        </div>
                     </div>
                 </div>
             )
