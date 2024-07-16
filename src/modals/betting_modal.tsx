9994c6d4aa6ce6f90b3366bf2114b502973ce499
DrRichadams <proxyserver7798@gmail.com>
2024-07-16 17:32:59 +0200

    We ended here from work on 16/07/24 -- 17:33

--- src/modals/betting_modal.tsx
+++ src/modals/betting_modal.tsx
@@ -1,8 +1,9 @@
 "use client"
 
 import React, { useState } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
 import styles from "./modal_styles.module.css";
+
 
 const BettingModal = () => {
     const [ selected, setSelected ] = useState("football");
@@ -10,19 +11,27 @@ const BettingModal = () => {
     const isBetting = searchParams.get("isBetting")
     const router = useRouter()
 
+    const pathname = usePathname();
+
     const handleModalClose = () => {
-        // alert("Modal has closed")
-        router.push("/football")
+        // alert(`${pathname}`)
+        router.push(`${pathname}`)
     }
 
         if(isBetting && isBetting=="open") {
             return (
                 <div className={styles.modalOverlay} onClick={handleModalClose}>
                     <div className={styles.modalContainer} onClick={(e) => e.stopPropagation()}>
-                        <div className={styles.bet_type_selector_box}>
-                            <button onClick={() => setSelected("football")} className={styles.bet_selector_btn} style={{ backgroundColor: selected=="football"? "#B1BF24":"transparent", color: selected=="football"? "#fff":"#B1BF24"  }}>Football</button>
-                            <button onClick={() => setSelected("survival")} className={styles.bet_selector_btn} style={{ backgroundColor: selected=="survival"? "#B1BF24":"transparent", color: selected=="survival"? "#fff":"#B1BF24" }}>Survival Pool</button>
+                        <div className={styles.bet_slip_title_bar}>
+                            BET SLIP
                         </div>
+                        <div className={styles.bet_slip_selector_bar}>
+                            SELECT BET TYPE
+                        </div>
+                        <div className={styles.bet_slip_fixtures}></div>
+                        <div className={styles.bet_slip_amount_placing}></div>
+                        <div className={styles.bet_slip_bet_details}></div>
+                        <button className={styles.bet_slip_place_bet_btn}>PLACE BET</button>
                     </div>
                 </div>
             )
